bdf9d75f5401a3f8ba9b202078b7f3b95024cea9
srirammaus <srirammaus@gmail.com>
2025-07-19 16:38:43 +0530

    some updates hosting it publicly

--- scripts/utils/apiConfig.js
+++ scripts/utils/apiConfig.js
@@ -1,20 +1,20 @@
 // import { uploadFile } from "../lib/webdrive.lib";
 
 export  default {
-    baseURL:"http://techxie.local:5000/",
-    userVerificationAPI:"http://techxie.local:5000/User/api/UserVerification",
-    signupAPI:"http://techxie.local:5000/User/api/signup",
-    emailVerificationURL:"http://techxie.local:5000/User/api/verifyMail",
-    authAPI:"http://techxie.local:5000/User/api/OAuth",
-    loginAPI:"http://techxie.local:5000/User/api/login",
-    tool_lstAPI:"http://techxie.local:5000/User/api/tools_lst",
-    csrfAPI:"http://techxie.local:5000/User/api/verify",
-    changePwd:"http://techxie.local:5000/User/api/changePwd",
-    driveUploadFile:"http://techxie.local:5000/user/file/upload",
-    createFolder:"http://techxie.local:5000/user/folder/createfolder",
-    delFolder:"http://techxie.local:5000/user/folder/delFolder",
-    delFile:"http://techxie.local:5000/user/file/delFile",
-    getFileInfo:"http://techxie.local:5000/user/file/getFileInfo",
-    pdfViewer:"http://techxie.local:5000/user/page/pdfViewer",
-    renameFolder: "http://techxie.local:5000/user/folder/rename"
+    baseURL:"http://https://techxie-4.onrender.com/",
+    userVerificationAPI:"http://https://techxie-4.onrender.com/User/api/UserVerification",
+    signupAPI:"http://https://techxie-4.onrender.com/User/api/signup",
+    emailVerificationURL:"http://https://techxie-4.onrender.com/User/api/verifyMail",
+    authAPI:"http://https://techxie-4.onrender.com/User/api/OAuth",
+    loginAPI:"http://https://techxie-4.onrender.com/User/api/login",
+    tool_lstAPI:"http://https://techxie-4.onrender.com/User/api/tools_lst",
+    csrfAPI:"http://https://techxie-4.onrender.com/User/api/verify",
+    changePwd:"http://https://techxie-4.onrender.com/User/api/changePwd",
+    driveUploadFile:"http://https://techxie-4.onrender.com/user/file/upload",
+    createFolder:"http://https://techxie-4.onrender.com/user/folder/createfolder",
+    delFolder:"http://https://techxie-4.onrender.com/user/folder/delFolder",
+    delFile:"http://https://techxie-4.onrender.com/user/file/delFile",
+    getFileInfo:"http://https://techxie-4.onrender.com/user/file/getFileInfo",
+    pdfViewer:"http://https://techxie-4.onrender.com/user/page/pdfViewer",
+    renameFolder: "http://https://techxie-4.onrender.com/user/folder/rename"
 }
